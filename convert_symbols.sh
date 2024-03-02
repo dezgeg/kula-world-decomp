49db7cf4ed0edf9d08a3246a466fd40ff0df5ed9
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-02 12:32:44 +0200

    More progress on link map

--- convert_symbols.sh
+++ convert_symbols.sh
@@ -1,1 +1,1 @@
-tail -n +3 | grep -v '^"switchdata' | grep -v '^"switch' | grep -v '^"case' | grep '"Function"' | tr '",' '  ' | awk '{ printf("%s = 0x%s; // type:func\n", $1, $2); }'
+tail -n +3 | grep -v '^"switchdata' | grep -v '^"switch' | grep -v '^"case' | grep '"Function"' | tr '",' '  ' | awk '{ printf("%s = 0x%s; // type:func\n", $1, $2); }' | grep -v '^gte_.* = 0x2'
