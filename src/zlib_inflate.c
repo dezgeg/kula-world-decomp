ffb78c44c051eff231c9f8768ed44fdbcf731503
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-10 17:01:55 +0200

    inflateEnd()

--- src/zlib_inflate.c
+++ src/zlib_inflate.c
@@ -62,3 +62,19 @@ z_streamp z;
   Trace((stderr, "inflate: reset\n"));
   return Z_OK;
 }
+
+
+int inflateEnd(z)
+z_streamp z;
+{
+  uLong c;
+
+  if (z == Z_NULL || z->state == Z_NULL || z->zfree == Z_NULL)
+    return Z_STREAM_ERROR;
+  if (z->state->blocks != Z_NULL)
+    inflate_blocks_free(z->state->blocks, z, &c);
+  ZFREE(z, z->state);
+  z->state = Z_NULL;
+  Trace((stderr, "inflate: end\n"));
+  return Z_OK;
+}
