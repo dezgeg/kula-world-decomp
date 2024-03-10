6d2a2f6c3e3749439f436ccca74cb223a5a98f17
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-10 22:23:30 +0200

    Yepp

--- src/zlib_inftrees.c
+++ src/zlib_inftrees.c
@@ -338,3 +338,73 @@ z_streamp z;            /* for zfree function */
   }
   return r;
 }
+
+
+int inflate_trees_dynamic(nl, nd, c, bl, bd, tl, td, z)
+uInt nl;                /* number of literal/length codes */
+uInt nd;                /* number of distance codes */
+uIntf *c;               /* that many (total) code lengths */
+uIntf *bl;              /* literal desired/actual bit depth */
+uIntf *bd;              /* distance desired/actual bit depth */
+inflate_huft * FAR *tl; /* literal/length tree result */
+inflate_huft * FAR *td; /* distance tree result */
+z_streamp z;            /* for zfree function */
+{
+  int r;
+#ifndef RODATA_IN_C
+  extern char S_oversubscribed_literal_length_tree[];
+  extern char S_incomplete_literal_length_tree[];
+#endif
+
+  /* build literal/length tree */
+  if ((r = huft_build(c, nl, 257, cplens, cplext, tl, bl, z)) != Z_OK)
+  {
+    if (r == Z_DATA_ERROR)
+#ifdef RODATA_IN_C
+      z->msg = (char*)"oversubscribed literal/length tree";
+#else
+      z->msg = (char*)S_oversubscribed_literal_length_tree;
+#endif
+    else if (r == Z_BUF_ERROR)
+    {
+      inflate_trees_free(*tl, z);
+#ifdef RODATA_IN_C
+      z->msg = (char*)"incomplete literal/length tree";
+#else
+      z->msg = (char*)S_incomplete_literal_length_tree;
+#endif
+      r = Z_DATA_ERROR;
+    }
+    return r;
+  }
+
+  /* build distance tree */
+  if ((r = huft_build(c + nl, nd, 0, cpdist, cpdext, td, bd, z)) != Z_OK)
+  {
+    if (r == Z_DATA_ERROR)
+#ifdef RODATA_IN_C
+      z->msg = (char*)"oversubscribed literal/length tree";
+#else
+      z->msg = (char*)S_oversubscribed_literal_length_tree;
+#endif
+    else if (r == Z_BUF_ERROR) {
+#ifdef PKZIP_BUG_WORKAROUND
+      r = Z_OK;
+    }
+#else
+      inflate_trees_free(*td, z);
+#ifdef RODATA_IN_C
+      z->msg = (char*)"incomplete literal/length tree";
+#else
+      z->msg = (char*)S_incomplete_literal_length_tree;
+#endif
+      r = Z_DATA_ERROR;
+    }
+    inflate_trees_free(*tl, z);
+    return r;
+#endif
+  }
+
+  /* done */
+  return Z_OK;
+}
