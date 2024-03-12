339181fa1a8631f5f5f78950965d9d1df0f841c4
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-12 20:51:23 +0200

    sigh

--- src/zlib_calloc.c
+++ src/zlib_calloc.c
@@ -21,3 +21,22 @@ void z_error(const char* msg) {
   while (1)
       ;
 }
+
+extern void* kulaZAllocatorPointer;
+extern long kulaZAllocatorHeapUsage;
+extern char kulaZAllocatorHeap[];
+
+void zcallocInit(void) {
+    __asm__(
+        "\t.extern kulaZAllocatorHeapUsage, 4\n"
+        "\t.extern kulaZAllocatorPointer, 4\n"
+        "\tlui        $a0,0xffe0\n"
+        "\tori        $a0,$a0,0xe000\n"
+        "\tlui        $v1,0x1f\n"
+        "\tlw         $v0,kulaZAllocatorPointer\n"
+        "\tori        $v1,$v1,0x2000\n"
+        "\tsw         $v1,kulaZAllocatorPointer\n"
+        "\taddu       $v0,$v0,$a0\n"
+        "\tsw         $v0,kulaZAllocatorHeapUsage\n"
+    );
+}
