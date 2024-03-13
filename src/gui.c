fbd0db10427e671ca0885f6b53e790f46f2d7574
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-13 18:58:00 +0200

    Another copy paste victory!

--- src/gui.c
+++ src/gui.c
@@ -3,6 +3,31 @@
 extern unsigned firstGuiTexture;
 extern Texture textures[];
 
+extern TSprite titleSprite[2];
+extern TSprite titleSprite_[2]; // hack to make gcc not create induction variable of sprt
+__asm__(".set titleSprite_, titleSprite");
+
+void InitTitleSprite(void)
+{
+    int i;
+    SPRT* sprt;
+    for (i = 0; i < 2; i++) {
+        TSpritePrim(&titleSprite[i], 0, 0, textures[firstGuiTexture + 16].tpage);
+        sprt = &titleSprite_[i].sprt;
+        setRGB0(sprt, 128,128,128);
+        sprt->x0 = 38;
+        sprt->y0 = 25;
+
+        SetSemiTrans(&titleSprite_[i].sprt, textures[firstGuiTexture + 16].semitrans);
+        SetShadeTex(&titleSprite_[i].sprt, 0);
+        titleSprite[i].sprt.clut = textures[firstGuiTexture + 16].clut;
+        titleSprite[i].sprt.u0 = textures[firstGuiTexture + 16].u;
+        titleSprite[i].sprt.v0 = textures[firstGuiTexture + 16].v;
+        titleSprite[i].sprt.w = textures[firstGuiTexture + 16].w;
+        titleSprite[i].sprt.h = textures[firstGuiTexture + 16].h;
+    }
+}
+
 extern TSprite copyrightSprite[2];
 extern TSprite copyrightSprite_[2]; // hack to make gcc not create induction variable of sprt
 __asm__(".set copyrightSprite_, copyrightSprite");
