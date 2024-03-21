9dc679937849b5f0ca911a3ccb9050ef0ee02a49
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-21 22:41:12 +0200

    Format zlib_calloc.c

--- src/zlib_calloc.c
+++ src/zlib_calloc.c
@@ -6,19 +6,19 @@ extern int whichDrawDispEnv;
 extern char kulaZAllocatorHeap[];
 
 void z_error(const char* msg) {
-  VSyncCallback(NULL);
-  SetupDisplay(1,128,0,0,0,0);
-  FntFlush(-1);
-  DrawSync(0);
-  whichDrawDispEnv = 0;
-  PutDrawAndDispEnvs();
-  FntPrint("Alloc error:\n\n");
-  FntPrint(msg);
-  FntFlush(-1);
-  whichDrawDispEnv = 1;
-  PutDrawAndDispEnvs();
-  while (1)
-      ;
+    VSyncCallback(NULL);
+    SetupDisplay(1, 128, 0, 0, 0, 0);
+    FntFlush(-1);
+    DrawSync(0);
+    whichDrawDispEnv = 0;
+    PutDrawAndDispEnvs();
+    FntPrint("Alloc error:\n\n");
+    FntPrint(msg);
+    FntFlush(-1);
+    whichDrawDispEnv = 1;
+    PutDrawAndDispEnvs();
+    while (1)
+        ;
 }
 
 void* kulaZAllocatorPointer;
@@ -33,65 +33,61 @@ void zcallocInit(void) {
         "\tori        $v1,$v1,0x2000\n"
         "\tsw         $v1,kulaZAllocatorPointer\n"
         "\taddu       $v0,$v0,$a0\n"
-        "\tsw         $v0,kulaZAllocatorHeapUsage\n"
-    );
+        "\tsw         $v0,kulaZAllocatorHeapUsage\n");
 }
 
 void* zcallocUnused(unsigned num, int size) {
-  char* ret = kulaZAllocatorPointer;
-  char* next = kulaZAllocatorPointer + num * size;
-  int i;
+    char* ret = kulaZAllocatorPointer;
+    char* next = kulaZAllocatorPointer + num * size;
+    int i;
 
-  kulaZAllocatorPointer = next;
-  if ((unsigned)next > 0x1fe000) {
-    VSyncCallback(NULL);
-    SetupDisplay(1,128,0,0,0,0);
-    FntFlush(-1);
-    DrawSync(0);
-    whichDrawDispEnv = 0;
-    PutDrawAndDispEnvs();
-    FntPrint("Alloc error:\n\n");
-    FntPrint("error in alloc, SIZE_ALLOC too small!\n");
-    FntFlush(-1);
-    whichDrawDispEnv = 1;
-    PutDrawAndDispEnvs();
-    while(1)
-        ;
-  }
+    kulaZAllocatorPointer = next;
+    if ((unsigned)next > 0x1fe000) {
+        VSyncCallback(NULL);
+        SetupDisplay(1, 128, 0, 0, 0, 0);
+        FntFlush(-1);
+        DrawSync(0);
+        whichDrawDispEnv = 0;
+        PutDrawAndDispEnvs();
+        FntPrint("Alloc error:\n\n");
+        FntPrint("error in alloc, SIZE_ALLOC too small!\n");
+        FntFlush(-1);
+        whichDrawDispEnv = 1;
+        PutDrawAndDispEnvs();
+        while (1)
+            ;
+    }
 
-  for(i = 0; i < (int)num * size; i++)
-      ret[i] = 0;
+    for (i = 0; i < (int)num * size; i++) ret[i] = 0;
 
-  return ret;
+    return ret;
 }
 
 void* zcalloc(void* opaque, unsigned num, int size) {
-  char* ret = kulaZAllocatorPointer;
-  char* next = kulaZAllocatorPointer + num * size;
-  int i;
+    char* ret = kulaZAllocatorPointer;
+    char* next = kulaZAllocatorPointer + num * size;
+    int i;
 
-  kulaZAllocatorPointer = next;
-  if ((unsigned)next > 0x1fe000) {
-    VSyncCallback(NULL);
-    SetupDisplay(1,128,0,0,0,0);
-    FntFlush(-1);
-    DrawSync(0);
-    whichDrawDispEnv = 0;
-    PutDrawAndDispEnvs();
-    FntPrint("Alloc error:\n\n");
-    FntPrint("error in alloc, SIZE_ALLOC too small!\n");
-    FntFlush(-1);
-    whichDrawDispEnv = 1;
-    PutDrawAndDispEnvs();
-    while(1)
-        ;
-  }
+    kulaZAllocatorPointer = next;
+    if ((unsigned)next > 0x1fe000) {
+        VSyncCallback(NULL);
+        SetupDisplay(1, 128, 0, 0, 0, 0);
+        FntFlush(-1);
+        DrawSync(0);
+        whichDrawDispEnv = 0;
+        PutDrawAndDispEnvs();
+        FntPrint("Alloc error:\n\n");
+        FntPrint("error in alloc, SIZE_ALLOC too small!\n");
+        FntFlush(-1);
+        whichDrawDispEnv = 1;
+        PutDrawAndDispEnvs();
+        while (1)
+            ;
+    }
 
-  for(i = 0; i < (int)num * size; i++)
-      ret[i] = 0;
+    for (i = 0; i < (int)num * size; i++) ret[i] = 0;
 
-  return ret;
+    return ret;
 }
 
-void zcfree(void* ptr) {
-}
+void zcfree(void* ptr) {}
