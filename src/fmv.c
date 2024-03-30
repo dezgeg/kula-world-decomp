e9a2f918d78697da74453fbece31ca54cf1d6772
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-30 20:50:03 +0200

    FMV stuff more

--- src/fmv.c
+++ src/fmv.c
@@ -5,6 +5,17 @@
 // Based on: PSX/SAMPLE/CD/MOVIE/TUTO1.C
 
 typedef struct {
+    char* fileName;
+    int is24bit;
+    int startFrame;
+    int endFrame;
+    int posX;
+    int posY;
+    int scrWidth;
+    int scrHeight;
+} MovieInfo;
+
+typedef struct {
     u_long* vlcbuf[2];
     int vlcid;
     u_short* imgbuf[2];
@@ -16,6 +27,47 @@ typedef struct {
     int is24bit;
 } DECENV;
 
+#define MOVIE_WAIT 2000
+#define VRAMPIX(pixels, is24bit)  ((is24bit) ? ((pixels) * 3) / 2 : (pixels))
+// weird variant? maybe the original doesn't work for PAL?
+#define VRAMPIX2(pixels, is24bit) ((is24bit) ? (pixels) * 3 : (pixels) << 1)
+
+int fmvEnded;
+int strWidth /* = 0*/;
+int strHeight /* = 0*/;
+
+u_long* StrNext(DECENV* dec, MovieInfo* movie) {
+    u_long* addr;
+    StHEADER* sector;
+    int cnt = MOVIE_WAIT;
+
+    while (StGetNext((u_long**)&addr, (u_long**)&sector)) {
+        if (--cnt == 0) return (0);
+    }
+
+    if ((int)sector->frameCount >= movie->endFrame) {
+        fmvEnded = 1;
+    }
+
+    if (strWidth != sector->width || strHeight != sector->height) {
+        RECT rect;
+        setRECT(&rect, 0, 0, VRAMPIX2(movie->scrWidth, movie->is24bit), movie->scrHeight);
+        if (movie->is24bit) {
+            ClearImage(&rect, 0, 0, 0);
+        } else {
+            ClearImage(&rect, 64, 64, 64);
+        }
+
+        strWidth = sector->width;
+        strHeight = sector->height;
+    }
+    dec->rect[0].w = dec->rect[1].w = VRAMPIX(strWidth, movie->is24bit);
+    dec->rect[0].h = dec->rect[1].h = strHeight;
+    dec->slice.h = strHeight;
+
+    return (addr);
+}
+
 void StrSync(DECENV* dec, int mode) {
     extern char S_time_out_in_decoding[];
     volatile u_long cnt = WAIT_TIME;
@@ -24,7 +76,7 @@ void StrSync(DECENV* dec, int mode) {
         if (--cnt == 0) {
             printf(S_time_out_in_decoding);
             dec->isdone = 1;
-            dec->rectid = dec->rectid? 0: 1;
+            dec->rectid = dec->rectid ? 0 : 1;
             dec->slice.x = dec->rect[dec->rectid].x;
             dec->slice.y = dec->rect[dec->rectid].y;
         }
