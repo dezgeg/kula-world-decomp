da9b739d600e21840e8f3c0ffdbfe62353683f59
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-30 19:43:38 +0200

    More fmv

--- src/fmv.c
+++ src/fmv.c
@@ -2,6 +2,36 @@
 
 #include <libcd.h>
 
+// Based on: PSX/SAMPLE/CD/MOVIE/TUTO1.C
+
+typedef struct {
+    u_long* vlcbuf[2];
+    int vlcid;
+    u_short* imgbuf[2];
+    int imgid;
+    RECT rect[2];
+    int rectid;
+    RECT slice;
+    int isdone;
+    int is24bit;
+} DECENV;
+
+void StrSync(DECENV* dec, int mode) {
+    extern char S_time_out_in_decoding[];
+    volatile u_long cnt = WAIT_TIME;
+
+    while (dec->isdone == 0) {
+        if (--cnt == 0) {
+            printf(S_time_out_in_decoding);
+            dec->isdone = 1;
+            dec->rectid = dec->rectid? 0: 1;
+            dec->slice.x = dec->rect[dec->rectid].x;
+            dec->slice.y = dec->rect[dec->rectid].y;
+        }
+    }
+    dec->isdone = 0;
+}
+
 void StrKickCd(CdlLOC* loc) {
     u_char param = CdlModeSpeed;
 
