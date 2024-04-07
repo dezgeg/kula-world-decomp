fe29338473fedf9a376f7897d8249e030d2430a2
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-07 20:09:33 +0300

    Hackery

--- src/music.c
+++ src/music.c
@@ -5,6 +5,9 @@
 
 extern void Noop(void);
 extern void Noop2(void);
+extern void PutDrawAndDispEnvs(void);
+extern void SetupDisplay(u_char isbg, u_char bgR, u_char bgG, u_char bgB, u_char useDithering, u_char use24Bit);
+extern void SndSetMusicVolume(void);
 
 int musicCounter;
 int musicCurSector;
@@ -15,10 +18,61 @@ int musicUnkAlwaysZero2;
 int savedMusicCurSector;
 int savedMusicEndSector;
 int savedMusicStartSector;
+int savedMusicXaChan;
+short playingBonusMusic;
 
 extern CdlFILTER musicCdlFilter;
+extern CdlLOC musicBonusLoc;
 extern CdlLOC musicCdlLoc;
 extern CdlLOC musicCurLoc;
+extern CdlLOC savedMusicCdlLoc;
+extern int musicCdMode;
+
+extern int whichDrawDispEnv;
+
+void SwitchFromBonusToNormalMusic(void) {
+    extern char S_File_error[];
+    extern char S_could_not_start_playing[];
+    extern char S_resumed_song_after_bonus[];
+    char dummy[8];
+
+    Noop2();
+    SndSetMusicVolume();
+    musicCdlLoc = savedMusicCdlLoc;
+    // XXX: Why?
+    *(volatile int*)&musicCurSector = savedMusicCurSector;
+    musicCounter = 50;
+    musicStartSector = savedMusicStartSector;
+    musicEndSector = savedMusicEndSector;
+    CdIntToPos(musicCurSector, &musicBonusLoc);
+    musicCdlFilter.chan = (u_char)savedMusicXaChan;
+    CdReadSync(0, 0);
+    while (CdControlB(CdlSetmode, &musicCdMode, 0) == 0)
+        ;
+    CdReadSync(0, 0);
+    while (CdControlB(CdlSetfilter, &musicCdlFilter, dummy) == 0)
+        ;
+    if (CdControlB(CdlReadS, &musicBonusLoc, dummy) == 0) {
+        VSyncCallback(NULL);
+        SetupDisplay(1, 0x80, 0, 0, 0, 0);
+        FntFlush(-1);
+        DrawSync(0);
+        whichDrawDispEnv = 0;
+        PutDrawAndDispEnvs();
+        FntPrint(S_File_error);
+        FntPrint(S_could_not_start_playing);
+        FntPrint(S_resumed_song_after_bonus);
+        FntFlush(-1);
+        whichDrawDispEnv = 1;
+        PutDrawAndDispEnvs();
+        do {
+        } while (1);
+    }
+    SndSetMusicVolume();
+    Noop();
+    SpuSetReverb(1);
+    playingBonusMusic = 0;
+}
 
 void UnusedDebugPrintMusicVariables(void) {
     extern char S_CD_INFO_STARTPOS_FMTd_ENDPOS_FMTd_CURPOS_FMTd[];
