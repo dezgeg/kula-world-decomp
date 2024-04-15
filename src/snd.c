e05448b94ebd6de04b827a32629f1437c54d2be2
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-15 17:10:37 +0300

    Finish snd.c

--- src/snd.c
+++ src/snd.c
@@ -10,12 +10,26 @@ typedef struct SpuVoiceState {
     int sfxIndex;
 } SpuVoiceState;
 
+typedef struct SfxFileEntry {
+    int offset;
+    int note;
+} SfxFileEntry;
+
+typedef struct SfxFile {
+    int count;
+    SfxFileEntry entries[1];
+} SfxFile;
+
 short playingBonusMusic;
 short bonusMusicIndex;
 
 SpuCommonAttr spuCommonAttr;
+SpuReverbAttr spuReverbAttr;
+ulong spuWriteRetVal;
 int sfxVolume;
 int musicVolume;
+int loadingIsComplete;
+int numSfx;
 
 extern SpuVoiceState spuVoiceState[];
 extern SpuVoiceAttr perSfxVoiceAttrs[];
@@ -38,11 +52,101 @@ static VECTOR* panVectorSqPtr = &panVectorSq;
 
 int voiceIter;
 
+void SndInitFromSfxFile(SfxFile* sfxFile, int length) {
+    uchar* addr;
+    int i;
+    int note;
+    int vol = 0x2aaa;
+    int off0;
+
+    loadingIsComplete = -1;
+    musicVolume = 8;
+    sfxVolume = 12;
+
+    spuCommonAttr.mask = 0x3cf;
+    spuCommonAttr.cd.volume.left = vol;
+    spuCommonAttr.cd.volume.right = 0x2aaa;
+    spuCommonAttr.cd.mix = 1;
+
+    spuReverbAttr.mode = 5;
+    spuReverbAttr.depth.left = 0x7fff;
+    spuReverbAttr.depth.right = 0x7fff;
+    spuReverbAttr.delay = 0x7fff;
+
+    spuCommonAttr.mvol.left = 0;
+    spuCommonAttr.mvol.right = 0;
+    spuCommonAttr.mvolmode.left = 0;
+    spuCommonAttr.mvolmode.right = 0;
+    spuCommonAttr.cd.reverb = 0;
+
+    spuReverbAttr.mask = 0;
+    spuReverbAttr.feedback = 0x2fff;
+
+    SsUtReverbOff();
+    SpuSetTransferStartAddr(0x1010);
+
+    addr = (int)sfxFile + sfxFile->entries[0].offset;
+    spuWriteRetVal = SpuWrite(addr, length - (int)addr);
+    SpuIsTransferCompleted(1);
+
+    numSfx = sfxFile->count;
+
+    SpuClearReverbWorkArea(spuReverbAttr.mode);
+    SpuIsTransferCompleted(1);
+    for (i = 0; i < sfxFile->count; i++) {
+        int off0 = sfxFile->entries[0].offset - 0x1010;
+        perSfxVoiceAttrs[i].addr = sfxFile->entries[i].offset - off0;
+        note = sfxFile->entries[i].note;
+        if ((note & 0x8000U) == 0) {
+            perSfxVoiceAttrs[i].note = note;
+            perSfxVoiceAttrs[i].sample_note = 0x2400;
+        } else {
+            perSfxVoiceAttrs[i].note = 0x2400;
+            perSfxVoiceAttrs[i].sample_note = note & 0x7fff;
+        }
+        perSfxVoiceAttrs[i].voice = 1 << i;
+        perSfxVoiceAttrs[i].mask = 0;
+        perSfxVoiceAttrs[i].volume.left = 0;
+        perSfxVoiceAttrs[i].volume.right = 0;
+        perSfxVoiceAttrs[i].volmode.left = 0;
+        perSfxVoiceAttrs[i].volmode.right = 0;
+        perSfxVoiceAttrs[i].volume.left = 0x3fff;
+        perSfxVoiceAttrs[i].volume.right = 0x3fff;
+        perSfxVoiceAttrs[i].pitch = 0x800;
+        perSfxVoiceAttrs[i].loop_addr = 0x10000;
+        perSfxVoiceAttrs[i].rr = 0x1f;
+        perSfxVoiceAttrs[i].envx = 0;
+        perSfxVoiceAttrs[i].a_mode = 1;
+        perSfxVoiceAttrs[i].s_mode = 3;
+        perSfxVoiceAttrs[i].r_mode = 3;
+        perSfxVoiceAttrs[i].ar = 0;
+        perSfxVoiceAttrs[i].dr = 0x7f;
+        perSfxVoiceAttrs[i].sr = 0x7f;
+        perSfxVoiceAttrs[i].sl = 0xf;
+        perSfxVoiceAttrs[i].adsr1 = 0;
+        perSfxVoiceAttrs[i].adsr2 = 0;
+    }
+    for (i = 0; i < 24; i++) {
+        spuVoiceState[i].sfxIndex = -1;
+    }
+
+    spuCommonAttr.mvol.left = 0x3fff;
+    spuCommonAttr.mvol.right = 0x3fff;
+    SpuSetCommonAttr(&spuCommonAttr);
+
+    SpuSetReverbModeParam(&spuReverbAttr);
+    SpuSetReverbDepth(&spuReverbAttr);
+    SpuSetReverb(1);
+    spuReverbAttr.mask = 6;
+    SpuSetReverbDepth(&spuReverbAttr);
+}
+
 void SndPlaySfx(int sfx, int tag, SVECTOR* dir, int volume) {
     int pan;
     curPlayingSfx = sfx;
 
-    do { } while (0);
+    do {
+    } while (0);
 
     if (curPlayingSfx < 120) {
         curPlayingSfx = SFX_REMAP_TABLE[sfx];
@@ -60,7 +164,8 @@ void SndPlaySfx(int sfx, int tag, SVECTOR* dir, int volume) {
             panFactor.vz = dir->vz;
             Square0(panFactorPtr, panVectorSqPtr);
             volume = (volume * sfxVolume) / 0xc;
-            sfxPanning = 3 * SquareRoot0(panVectorSqPtr->vx + panVectorSqPtr->vy + panVectorSqPtr->vz);
+            sfxPanning =
+                3 * SquareRoot0(panVectorSqPtr->vx + panVectorSqPtr->vy + panVectorSqPtr->vz);
 
             if (sfxPanning > volume) {
                 sfxPanning = volume;
@@ -71,14 +176,18 @@ void SndPlaySfx(int sfx, int tag, SVECTOR* dir, int volume) {
             } else {
                 if (sndSwapPanDir) {
                     perSfxVoiceAttrs[curPlayingSfx].volume.left =
-                        (volume - sfxPanning) + ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
+                        (volume - sfxPanning) +
+                        ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
                     perSfxVoiceAttrs[curPlayingSfx].volume.right =
-                        (volume - sfxPanning) - ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
+                        (volume - sfxPanning) -
+                        ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
                 } else {
                     perSfxVoiceAttrs[curPlayingSfx].volume.right =
-                        (volume - sfxPanning) + ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
+                        (volume - sfxPanning) +
+                        ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
                     perSfxVoiceAttrs[curPlayingSfx].volume.left =
-                        (volume - sfxPanning) - ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
+                        (volume - sfxPanning) -
+                        ((panFactorPtr->vx * (volume - sfxPanning)) / sfxPanning);
                 }
             }
             perSfxVoiceAttrs[curPlayingSfx].mask = 0;
