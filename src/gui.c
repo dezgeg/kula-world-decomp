6b1ad3ed80f88062888f9d2306e32008b25dea6b
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-28 01:31:22 +0300

    Remove hacks Pog

--- src/gui.c
+++ src/gui.c
@@ -6,21 +6,17 @@ int drawTimerPausedWidget;
 extern Texture textures[];
 
 extern TSprite titleSprite[2];
-extern TSprite titleSprite_[2];  // hack to make gcc not create induction variable of sprt
-__asm__(".set titleSprite_, titleSprite");
+extern TSprite copyrightSprite[2];
+extern TSprite timerPausedSprite[2];
 
 void InitTitleSprite(void) {
     int i;
-    SPRT* sprt;
     for (i = 0; i < 2; i++) {
         TSpritePrim(&titleSprite[i], 0, 0, textures[firstGuiTexture + 16].tpage);
-        sprt = &titleSprite_[i].sprt;
-        setRGB0(sprt, 128, 128, 128);
-        sprt->x0 = 38;
-        sprt->y0 = 25;
-
-        SetSemiTrans(&titleSprite_[i].sprt, textures[firstGuiTexture + 16].semitrans);
-        SetShadeTex(&titleSprite_[i].sprt, 0);
+        setRGB0(&titleSprite[i].sprt, 128, 128, 128);
+        setXY0(&titleSprite[i].sprt, 38, 25);
+        SetSemiTrans(&titleSprite[i].sprt, textures[firstGuiTexture + 16].semitrans);
+        SetShadeTex(&titleSprite[i].sprt, 0);
         titleSprite[i].sprt.clut = textures[firstGuiTexture + 16].clut;
         titleSprite[i].sprt.u0 = textures[firstGuiTexture + 16].u;
         titleSprite[i].sprt.v0 = textures[firstGuiTexture + 16].v;
@@ -29,22 +25,14 @@ void InitTitleSprite(void) {
     }
 }
 
-extern TSprite copyrightSprite[2];
-extern TSprite copyrightSprite_[2];  // hack to make gcc not create induction variable of sprt
-__asm__(".set copyrightSprite_, copyrightSprite");
-
 void InitCopyrightSprite(void) {
     int i;
-    SPRT* sprt;
     for (i = 0; i < 2; i++) {
         TSpritePrim(&copyrightSprite[i], 0, 0, textures[firstGuiTexture + 17].tpage);
-        sprt = &copyrightSprite_[i].sprt;
-        setRGB0(sprt, 128, 128, 128);
-        sprt->x0 = 46;
-        sprt->y0 = 218;
-
-        SetSemiTrans(&copyrightSprite_[i].sprt, textures[firstGuiTexture + 17].semitrans);
-        SetShadeTex(&copyrightSprite_[i].sprt, 0);
+        setRGB0(&copyrightSprite[i].sprt, 128, 128, 128);
+        setXY0(&copyrightSprite[i].sprt, 46, 218);
+        SetSemiTrans(&copyrightSprite[i].sprt, textures[firstGuiTexture + 17].semitrans);
+        SetShadeTex(&copyrightSprite[i].sprt, 0);
         copyrightSprite[i].sprt.clut = textures[firstGuiTexture + 17].clut;
         copyrightSprite[i].sprt.u0 = textures[firstGuiTexture + 17].u;
         copyrightSprite[i].sprt.v0 = textures[firstGuiTexture + 17].v;
@@ -53,24 +41,16 @@ void InitCopyrightSprite(void) {
     }
 }
 
-extern TSprite timerPausedSprite[2];
-extern TSprite timerPausedSprite_[2];  // hack to make gcc not create induction variable of sprt
-__asm__(".set timerPausedSprite_, timerPausedSprite");
-
 void InitTimerPausedSprite(void) {
     int i;
-    SPRT* sprt;
 
     drawTimerPausedWidget = 0;
     for (i = 0; i < 2; i++) {
         TSpritePrim(&timerPausedSprite[i], 0, 0, textures[firstGuiTexture + 19].tpage);
-        sprt = &timerPausedSprite_[i].sprt;
-        setRGB0(sprt, 128, 128, 128);
-        sprt->x0 = 140;
-        sprt->y0 = 6;
-
-        SetSemiTrans(&timerPausedSprite_[i].sprt, textures[firstGuiTexture + 19].semitrans);
-        SetShadeTex(&timerPausedSprite_[i].sprt, 0);
+        setRGB0(&timerPausedSprite[i].sprt, 128, 128, 128);
+        setXY0(&timerPausedSprite[i].sprt, 140, 6);
+        SetSemiTrans(&timerPausedSprite[i].sprt, textures[firstGuiTexture + 19].semitrans);
+        SetShadeTex(&timerPausedSprite[i].sprt, 0);
         timerPausedSprite[i].sprt.clut = textures[firstGuiTexture + 19].clut;
         timerPausedSprite[i].sprt.u0 = textures[firstGuiTexture + 19].u;
         timerPausedSprite[i].sprt.v0 = textures[firstGuiTexture + 19].v;
