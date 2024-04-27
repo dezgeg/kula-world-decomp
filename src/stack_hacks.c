6e11478c7f83f17653c50dbb3d5d3751e48e822e
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-28 01:14:43 +0300

    Finish stack_hacks

--- src/stack_hacks.c
+++ src/stack_hacks.c
@@ -2,23 +2,35 @@
 
 #include <libapi.h>
 
-extern uint savedSp;
-extern uint unusedInitStackParamEnd;
-extern uint unusedInitStackParamStart;
+extern uint* savedSp;
+extern uint* unusedInitStackParamEnd;
+extern uint* unusedInitStackParamStart;
 
 void UnusedInitStack(uint end, uint start) {
-    uint endMasked = end & 0xfffffff;
-    int* p;
+    uint* endMasked = end & 0xfffffff;
+    uint* startMasked = start & 0xfffffff;
+    uint* p;
     int val;
 
     unusedInitStackParamEnd = endMasked;
-    unusedInitStackParamStart = start & 0xfffffff;
+    unusedInitStackParamStart = startMasked;
 
     p = (int*)(GetSp() & 0xfffffff);
     val = 0x12345678;
-    savedSp = (uint)p;
+    savedSp = p;
 
     while (endMasked <= --p) {
         *p = val++;
     }
+}
+
+int UnusedValidateStack(void) {
+    uint val;
+    uint* p = unusedInitStackParamEnd;
+
+    val = (savedSp - unusedInitStackParamEnd) + 0x12345678;
+    while (p <= unusedInitStackParamStart && *p++ == --val)
+        ;
+
+    return unusedInitStackParamStart - p;
 }
