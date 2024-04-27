beace849954ecf0f8315a50fc3975af27634cef9
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-27 23:36:13 +0300

    begin fancy WTFs

---
+++ src/fancy_text.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+int fancyTextColorB;
+int fancyTextColorG;
+int fancyTextColorR;
+
+void SetFancyTextColor(int r, int g, int b) {
+    fancyTextColorR = r;
+    fancyTextColorG = g;
+    fancyTextColorB = b;
+}
