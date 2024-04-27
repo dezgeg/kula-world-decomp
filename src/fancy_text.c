7d1ef1136b136f5047bfdd846956cc3ab886a6cb
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-04-28 00:35:41 +0300

    ResetTextVars

--- src/fancy_text.c
+++ src/fancy_text.c
@@ -3,9 +3,17 @@
 int fancyTextColorB;
 int fancyTextColorG;
 int fancyTextColorR;
+int textCharIdx;
 
 void SetFancyTextColor(int r, int g, int b) {
     fancyTextColorR = r;
     fancyTextColorG = g;
     fancyTextColorB = b;
+}
+
+void ResetTextVars(void) {
+    textCharIdx = 0;
+    fancyTextColorR = 0;
+    fancyTextColorG = 0;
+    fancyTextColorB = 0;
 }
