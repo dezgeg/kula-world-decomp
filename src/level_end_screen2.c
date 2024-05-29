44e214954bb7d723f94e486c949fe4d4654a2e3d
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-05-29 19:52:44 +0300

    Back at it again

---
+++ src/level_end_screen2.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+int bigGuiSpriteFade;
+
+void FadeOutBigGuiSprite(void) {
+    bigGuiSpriteFade -= 20;
+    if (bigGuiSpriteFade < 0) {
+        bigGuiSpriteFade = 0;
+    }
+}
