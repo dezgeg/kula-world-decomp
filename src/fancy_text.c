90cdad1d69623f4c847167658f3f64645d2d9175
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-06-16 19:22:38 +0300

    DrawTextFancyFont

--- src/fancy_text.c
+++ src/fancy_text.c
@@ -103,3 +103,150 @@ void DrawStaticUiSprite(short id, short x, short y, short count) {
         textCharIdx++;
     }
 }
+
+void DrawTextFancyFont(char *str,short x,short y) {
+    int startIdx;
+    int endIdx;
+    int finalX;
+    int finalY;
+    int fade;
+    int r;
+    int g;
+    int b;
+    int col;
+
+    startIdx = 0;
+    endIdx = 0;
+    finalY = y;
+    for (; str[startIdx]; startIdx = endIdx + 1) {
+        endIdx = startIdx;
+        while (str[endIdx] != '\0' && str[endIdx] != '\n') {
+            endIdx++;
+        }
+        finalX = x + (endIdx - startIdx) * -12;
+        r = bigGuiSpriteFade - finalY / 2 - fancyTextColorR;
+        g = bigGuiSpriteFade - finalY / 2 - fancyTextColorG;
+        b = bigGuiSpriteFade - finalY / 2 - fancyTextColorB;
+        col = bigGuiSpriteFade - finalY / 2;
+        if (r < 0) {
+            r = 0;
+        }
+        if (r > 0x80) {
+            r = 0x80;
+        }
+
+        if (g < 0) {
+            g = 0;
+        }
+        if (g > 0x80) {
+            g = 0x80;
+        }
+
+        if (b < 0) {
+            b = 0;
+        }
+        if (b > 0x80) {
+            b = 0x80;
+        }
+
+        if (col < 0) {
+            col = 0;
+        }
+        if (col > 0x80) {
+            col = 0x80;
+        }
+        
+        while (startIdx < endIdx) {
+            if (str[startIdx] - 0x30 < 10u) {
+                if (textCharIdx + 2 > 299) {
+                    return;
+                }
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites1[str[startIdx] - 0x30];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, col, col, col);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites2[str[startIdx] - 0x30];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, r, g, b);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+            }
+            if (str[startIdx] == '+') {
+                if (textCharIdx + 2 > 299) {
+                    return;
+                }
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites1[10];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, col, col, col);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites2[10];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, r, g, b);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+            }
+            if (str[startIdx] == '-') {
+                if (textCharIdx + 2 > 299) {
+                    return;
+                }
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites1[11];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, col, col, col);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites2[11];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, r, g, b);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+            }
+            if (str[startIdx] == '=') {
+                if (textCharIdx + 2 > 299) {
+                    return;
+                }
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites1[12];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, col, col, col);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites2[12];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, r, g, b);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+            }
+            if (str[startIdx] == ':') {
+                if (textCharIdx + 2 > 299) {
+                    return;
+                }
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites1[14];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, col, col, col);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+
+                textDrawSprites[whichDrawDispEnv][textCharIdx] = textDrawTemplateSprites2[14];
+                setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, r, g, b);
+                setXY0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, finalX, finalY);
+                addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                textCharIdx++;
+            }
+            startIdx++;
+            finalX += 12;
+        }
+
+        finalY += 18;
+        startIdx = endIdx;
+        startIdx++;
+        if (!str[endIdx])
+            return;
+    }
+}
