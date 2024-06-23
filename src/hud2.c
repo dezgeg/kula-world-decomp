107142165d74b7847f8e2eb7846627efcae69627
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-06-23 12:30:18 +0300

    DrawKeyWidgets

---
+++ src/hud2.c
@@ -0,0 +1,22 @@
+#include "common.h"
+
+extern TSprite keySprites[2][4][2];
+extern int numKeysRemaining;
+extern PrimList primLists[2];
+extern int whichDrawDispEnv;
+
+int numKeysInLevel;
+
+void DrawKeyWidgets(void) {
+    int i;
+
+    for (i = numKeysInLevel - 1; i >= 0; i--) {
+        if (i < numKeysInLevel - numKeysRemaining){
+            addPrim(&primLists[whichDrawDispEnv].gui1,
+                    &keySprites[whichDrawDispEnv][i][1]);
+        } else {
+            addPrim(&primLists[whichDrawDispEnv].gui1,
+                    &keySprites[whichDrawDispEnv][i][0]);
+        }
+    }
+}
