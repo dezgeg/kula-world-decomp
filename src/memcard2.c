c4dc4737b2016b777f8b6d5562e93c4c55179027
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-11-02 22:24:00 +0200

    Split tweaking

--- src/memcard2.c
+++ src/memcard2.c
@@ -8,11 +8,13 @@ int memCardDataValid;
 long mcResult;
 long tempMcResult;
 
+extern DrawDisp drawdisp[2];
 extern Highscore highscores[6];
-extern MemcardData memCardData;
 extern int curLevel;
 extern int curWorld;
 extern int dispenvScreenX;
+extern int dispenvScreenX;
+extern int dispenvScreenY;
 extern int dispenvScreenY;
 extern int finalUnlocked;
 extern int gameMode;
@@ -27,10 +29,16 @@ extern int totalPlayTime[2];
 extern int totalScore;
 extern int vibrationEnabled;
 extern long mcCmd;
+extern MemcardData memCardData;
 extern short numFruits;
 extern short turnDelayEnabled;
 extern uint fruitsCollectedBitmask;
 extern uint savedFruitsCollectedBitmask;
+
+static inline void SetDispScreen(ushort x, ushort y) {
+    drawdisp[0].disp.screen.x = drawdisp[1].disp.screen.x = x;
+    drawdisp[0].disp.screen.y = drawdisp[1].disp.screen.y = y;
+}
 
 static inline void ClearMemCardData() {
     uint i;
@@ -177,4 +185,42 @@ int LoadSaveSlot(uint slot) {
         fruitsCollectedBitmask = savedFruitsCollectedBitmask;
     }
     return 1;
+}
+
+INCLUDE_ASM("asm/nonmatchings/memcard2", SaveMemCard);
+
+void LoadSaveFromMemoryCard(void){
+    extern char S_BESCES_01000KULA[];
+    uint i;
+    char* p = &memCardData;
+
+    for (i = 0; i < 8196; i++) {
+        *p++ = 0;
+    }
+    MemCardAccept(0);
+    MemCardSync(0, &mcCmd, &tempMcResult);
+    if ((tempMcResult == 0) || (tempMcResult == McErrNewCard)) {
+        tempMcResult = McErrCardNotExist;
+        if (MemCardReadFile(0, S_BESCES_01000KULA, (unsigned long*)&memCardData, 0, 0x1000)) {
+            MemCardSync(0, &mcCmd, &tempMcResult);
+        }
+        if (tempMcResult == 0) {
+            if (memCardData.arcadeModeDone == McErrCardNotExist) {
+                finalUnlocked = McErrCardNotExist;
+            }
+            if (highestLevelReached < memCardData.highestLevelReached) {
+                highestLevelReached = (int)memCardData.highestLevelReached;
+            }
+            turnDelayEnabled = memCardData.turnDelayEnabled;
+            vibrationEnabled = memCardData.vibrationEnabled;
+            musicVolume = memCardData.musicVolume;
+            sfxVolume = memCardData.sfxVolume;
+            dispenvScreenX = memCardData.screenX;
+            dispenvScreenY = memCardData.screenY;
+            LoadHighscoresFromMemcardData();
+            memCardDataValid = 1;
+            SetDispScreen(dispenvScreenX, dispenvScreenY);
+            mcResult = tempMcResult;
+        }
+    }
 }
