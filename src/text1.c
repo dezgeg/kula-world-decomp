c6fb50913fa242758b11b38e146551c22cb918db
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-11-04 19:29:30 +0200

    Finish text1

--- src/text1.c
+++ src/text1.c
@@ -1,11 +1,19 @@
 #include "common.h"
 
-int lastFrameIdWhenTextWasDrawn;
-int textAlignMode;
+extern TSprite fontSprites[96];
+extern PrimList primLists[2];
+extern TSprite textDrawSprites[2][300];
+extern int whichDrawDispEnv;
+
 byte textColorB;
 byte textColorG;
 byte textColorR;
+int lastFrameIdWhenTextWasDrawn;
+int textAlignMode;
+int textCharIdx;
+int textPosXEnd;
 int textPosXStart;
+int textPosYCur;
 int textPosYEnd;
 int textPosYStart;
 
@@ -44,5 +52,56 @@ void SetTextParams(int posX, int posY, int align, int colorR, int colorG, int co
     }
 }
 
-// https://decomp.me/scratch/VgNU6
-INCLUDE_ASM("asm/nonmatchings/text1", DrawTextCrappyFont);
+void DrawTextCrappyFont(char *str) {
+    int i;
+    int j;
+    int width;
+    int lineStartIndex;
+
+    if (whichDrawDispEnv != lastFrameIdWhenTextWasDrawn) {
+        lastFrameIdWhenTextWasDrawn = whichDrawDispEnv;
+        textPosXEnd = textPosXStart;
+        textPosYCur = textPosYStart;
+        textPosYEnd = textPosYStart;
+    }
+
+    textPosYCur = textPosYEnd;
+    lineStartIndex = 0;
+    for (width = i = 0; str[i]; i++) {
+        width += fontSprites[str[i] - 0x20].sprt.w + 1;
+        if (str[i] == '\n' || str[i] == 0) {
+            if (textAlignMode == 0) {
+                textPosXEnd = textPosXStart;
+            }
+            if (textAlignMode == 1) {
+                textPosXEnd = textPosXStart - width / 2;
+            }
+            if (textAlignMode == 2) {
+                textPosXEnd = textPosXStart - width;
+            }
+
+            for (j = lineStartIndex; str[j] != '\n' && str[j] ;j++) {
+                if (fontSprites[str[j] - 0x20].sprt.h != 0 && str[j] - 0x20 >= 0) {
+                    if (textCharIdx + 1 >= 400) {
+                        return;
+                    }
+                    memcpy(&textDrawSprites[whichDrawDispEnv][textCharIdx], &fontSprites[str[j] - 0x20], sizeof(TSprite));
+                    if (str[j] != '|') {
+                        setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, textColorR, textColorG, textColorB);
+                    } else {
+                        setRGB0(&textDrawSprites[whichDrawDispEnv][textCharIdx].sprt, textColorR, 0, 0);
+                    }
+                    textDrawSprites[whichDrawDispEnv][textCharIdx].sprt.x0 += textPosXEnd;
+                    textDrawSprites[whichDrawDispEnv][textCharIdx].sprt.y0 += textPosYCur;
+                    addPrim(&primLists[whichDrawDispEnv].main, &textDrawSprites[whichDrawDispEnv][textCharIdx]);
+                    textCharIdx++;
+                }
+                textPosXEnd += fontSprites[str[j] - 0x20].sprt.w + 1;
+            }
+            width = 0;
+            lineStartIndex = i + 1;
+            textPosYCur += 11;
+        }
+    }
+    textPosYEnd = textPosYCur;
+}
