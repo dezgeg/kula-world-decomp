072909d06328ebcb98ff88b77141353f1fa7753c
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 18:23:56 +0200

    Migrate EnableTurningMotionBlur, ProcessTurningMotionBlur, TurnLevelExitQuadIntoGreen from pending_matches

--- src/update.c
+++ src/update.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+extern TgiFile * tgi;
+extern int turningMotionBlurEnabled;
+extern int turningMotionBlurPhase;
+extern int turningMotionBlurTable[70];
+extern int numKeysRemaining;
+extern Quad** levelExitQuadPPtr;
+
 INCLUDE_ASM("asm/nonmatchings/update", ProcessMovingPlatforms2);
 
 INCLUDE_ASM("asm/nonmatchings/update", ProcessCrumblingBlocks);
@@ -12,10 +19,40 @@ INCLUDE_ASM("asm/nonmatchings/update", SetSunglassMode);
 
 INCLUDE_ASM("asm/nonmatchings/update", UpdateSunglassModeDisabling);
 
-INCLUDE_ASM("asm/nonmatchings/update", EnableTurningMotionBlur);
+void EnableTurningMotionBlur(void) {
+    if (turningMotionBlurEnabled == 0) {
+        turningMotionBlurEnabled = 1;
+        turningMotionBlurPhase = 0;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/update", ProcessTurningMotionBlur);
+void ProcessTurningMotionBlur(void) {
+    int i;
+    
+    if (turningMotionBlurEnabled != 0) {
+        if (turningMotionBlurEnabled == 1) {
+            turningMotionBlurPhase++;
+            if (turningMotionBlurPhase == 9) {
+                turningMotionBlurPhase -= 2;
+                turningMotionBlurEnabled = 2;
+            }
+        } else {
+            turningMotionBlurPhase--;
+            if (turningMotionBlurPhase == 0) {
+                turningMotionBlurEnabled = 0;
+            }
+        }
+        
+        for (i = 0; i < 7; i++) {
+            tgi->lodDistance[i] = turningMotionBlurTable[turningMotionBlurPhase * 7 + i];
+        }
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/update", TurnLevelExitQuadIntoGreen);
+void TurnLevelExitQuadIntoGreen(void) {
+    if (numKeysRemaining == 0) {
+        ((byte*)&(*levelExitQuadPPtr)->flags)[3] = 1;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/update", DisableItemShadow);
