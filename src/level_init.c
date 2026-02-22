04ae957bfab7d0a2784408a0283a982caae7bfef
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-23 00:17:15 +0200

    Integrate

--- src/level_init.c
+++ src/level_init.c
@@ -2,6 +2,20 @@
 
 extern uint Rand(int param_1);
 
+typedef struct P {
+    ulong* p0;
+    ulong* p1;
+} P;
+
+extern POLY_FT4 shadowPrims[2][1][2][16];
+extern POLY_FT4 specularPrims[2][1][16];
+extern P shadowPrimPtrs[2][1][2][16];
+extern P specularPrimPtrs[2][1][16];
+extern Texture textures[150];
+extern Texture textures[150];
+extern uint firstGuiTexture;
+extern uint firstGuiTexture;
+
 TgiFile* tgi;
 
 INCLUDE_ASM("asm/nonmatchings/level_init", ProcessLevelData);
@@ -68,8 +82,60 @@ INCLUDE_ASM("asm/nonmatchings/level_init", AddQuadToAnimatedTextureChain);
 
 INCLUDE_ASM("asm/nonmatchings/level_init", FUN_000298e0);
 
-INCLUDE_ASM("asm/nonmatchings/level_init", InitShadowSprites);
+void InitShadowSprites(void) {
+    int i;
+    int j;
+    int k;
+    int l;
+    int tex;
 
-INCLUDE_ASM("asm/nonmatchings/level_init", InitPlayerSpecularSprite);
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 1; j++) {
+            for (k = 0; k < 2; k++) {
+                for (l = 0; l < 16; l++) {
+                    tex = k + 14;
+                    shadowPrimPtrs[i][j][k][l].p0 = 0xffffffff;
+                    setPolyFT4(&shadowPrims[i][j][k][l]);
+                    setUV4(&shadowPrims[i][j][k][l],
+                            textures[firstGuiTexture + tex].u, textures[firstGuiTexture + tex].v,
+                            textures[firstGuiTexture + tex].u + textures[firstGuiTexture + tex].w - 1, textures[firstGuiTexture + tex].v,
+                            textures[firstGuiTexture + tex].u, textures[firstGuiTexture + tex].v + textures[firstGuiTexture + tex].h - 1,
+                            textures[firstGuiTexture + tex].u + textures[firstGuiTexture + tex].w - 1, textures[firstGuiTexture + tex].v + textures[firstGuiTexture + tex].h - 1);
+                    SetSemiTrans(&shadowPrims[i][j][k][l], textures[firstGuiTexture + tex].semitrans);
+                    SetShadeTex(&shadowPrims[i][j][k][l], 0);
+                    shadowPrims[i][j][k][l].tpage = textures[firstGuiTexture + tex].tpage;
+                    shadowPrims[i][j][k][l].clut = textures[firstGuiTexture + tex].clut;
+                }
+            }
+        }
+    }
+
+
+}
+
+void InitPlayerSpecularSprite(void) {
+    int i;
+    int j;
+    int k;
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 1; j++) {
+            for (k = 0; k < 16; k++) {
+                specularPrimPtrs[i][j][k].p0 = 0xffffffff;
+                setPolyFT4(&specularPrims[i][j][k]);
+                setUV4(&specularPrims[i][j][k],
+                        textures[firstGuiTexture + 18].u, textures[firstGuiTexture + 18].v,
+                        textures[firstGuiTexture + 18].u + textures[firstGuiTexture + 18].w - 1, textures[firstGuiTexture + 18].v,
+                        textures[firstGuiTexture + 18].u, textures[firstGuiTexture + 18].v + textures[firstGuiTexture + 18].h - 1,
+                        textures[firstGuiTexture + 18].u + textures[firstGuiTexture + 18].w - 1, textures[firstGuiTexture + 18].v + textures[firstGuiTexture + 18].h - 1);
+                SetSemiTrans(&specularPrims[i][j][k], textures[firstGuiTexture + 18].semitrans);
+                SetShadeTex(&specularPrims[i][j][k], 0);
+                setRGB0(&specularPrims[i][j][k], 0x80, 0x80, 0x80);
+                specularPrims[i][j][k].tpage = textures[firstGuiTexture + 18].tpage;
+                specularPrims[i][j][k].clut = textures[firstGuiTexture + 18].clut;
+            }
+        }
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/level_init", ScanLevelDataForBlinkingEntities);
