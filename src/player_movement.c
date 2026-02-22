8233a12f036d99e71c546a863014f1c3e8da0593
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 18:38:07 +0200

    Migrate TurnLeft, TurnRight from pending_matches

--- src/player_movement.c
+++ src/player_movement.c
@@ -9,6 +9,7 @@ short *initJumpTimerPtr;
 extern short *ggiPart5JumpAnimData;
 
 extern void JumpingOnMovingPlatform(Player *player);
+extern void EnableTurningMotionBlur(void);
 
 void ResetPlayerVars(Player *player) {
     short* anim = ggiPart5JumpAnimData;
@@ -101,9 +102,39 @@ void StartJumpingInplace(Player *player) {
     player->jumpdataPtr += 4;
 }
 
-INCLUDE_ASM("asm/nonmatchings/player_movement", TurnRight);
+void TurnRight(Player *player) {
+    int pad[2];
 
-INCLUDE_ASM("asm/nonmatchings/player_movement", TurnLeft);
+    if (player->startTurningTo == 0 && player->field119_0x1c8 == 0) {
+        player->unusedTurningWhere = 1;
+        player->startTurningTo = 1;
+        player->turningTimer = 16;
+        player->turningDelta = -64;
+        player->onGround = 0;
+        player->howMoving0 = 1;
+        if (player->turningRightRelated >= 2) {
+            player->turningRightRelated = 3;
+        }
+        EnableTurningMotionBlur();
+    }
+}
+
+void TurnLeft(Player *player) {
+    int pad[2];
+
+    if (player->startTurningTo == 0 && player->field119_0x1c8 == 0) {
+        player->unusedTurningWhere = 2;
+        player->startTurningTo = 2;
+        player->turningTimer = 16;
+        player->turningDelta = 64;
+        player->onGround = 0;
+        player->howMoving0 = 1;
+        if (player->turningLeftRelated >= 2) {
+            player->turningLeftRelated = 3;
+        }
+        EnableTurningMotionBlur();
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/player_movement", ProcessMovement);
 
