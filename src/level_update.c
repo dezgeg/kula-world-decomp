e22277615819209973b934d24ca5b882e787d931
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 18:31:11 +0200

    Migrate GetBlockAt from pending_matches

--- src/level_update.c
+++ src/level_update.c
@@ -5,6 +5,12 @@ int curController;
 short numCopycatMoves;
 short unusedNumCopycatRounds;
 
+int getBlockX;
+int getBlockY;
+int getBlockZ;
+int getBlockResult;
+
+extern short* levelData;
 extern short copycatMoves[1024];
 extern short copycatNewOrCopyMoves;
 
@@ -74,7 +80,28 @@ INCLUDE_ASM("asm/nonmatchings/level_update", HandlePlayerButtons);
 
 INCLUDE_ASM("asm/nonmatchings/level_update", CalcWhatPlayerIsStandingOn);
 
-INCLUDE_ASM("asm/nonmatchings/level_update", GetBlockAt);
+#define CUBE_TYPE_AT(x, y, z) levelData[(x) * 1156 + (y) * 34 + (z)]
+
+int GetBlockAt(SVECTOR *coord) {
+    int x, y, z;
+    int res;
+    
+    x = (coord->vx + 0x100) >> 9;
+    y = (coord->vy + 0x100) >> 9;
+    z = (coord->vz + 0x100) >> 9;
+    
+    getBlockX = x;
+    getBlockY = y;
+    getBlockZ = z;
+    
+    if (x < 1 || y < 1 || z < 1 || x > 32 || y > 32 || z > 32) {
+        return -1;
+    } else {
+        res = CUBE_TYPE_AT(x, y, z);
+        getBlockResult = res;
+        return res;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/level_update", UpdateSubpixelPositions);
 
