bacaaa2c79ec8a33ed6cd5d7a77a3123154ffe3b
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 18:46:43 +0200

    Migrate FUN_000344b0 from pending_matches

--- src/level_update.c
+++ src/level_update.c
@@ -57,7 +57,12 @@ INCLUDE_ASM("asm/nonmatchings/level_update", JumpingOnMovingPlatform);
 
 INCLUDE_ASM("asm/nonmatchings/level_update", FUN_0003418c);
 
-INCLUDE_ASM("asm/nonmatchings/level_update", FUN_000344b0);
+int FUN_000344b0(int a0, int a1) {
+    if ((a0 == 1 || a0 == 4) && (a1 == 1 || a1 == 4)) return 1;
+    if ((a0 == 2 || a0 == 3) && (a1 == 2 || a1 == 3)) return 1;
+    if ((a0 == 0 || a0 == 5) && (a1 == 0 || a1 == 5)) return 1;
+    return 0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/level_update", FUN_00034518);
 
