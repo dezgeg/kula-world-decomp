f9400534474f27698a0308d3c1ede1ac66f6954d
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-23 00:05:25 +0200

    CoordHash

--- src/level_init.c
+++ src/level_init.c
@@ -8,7 +8,28 @@ INCLUDE_ASM("asm/nonmatchings/level_init", ProcessLevelData);
 
 INCLUDE_ASM("asm/nonmatchings/level_init", ProcessCubesIntoFaces);
 
-INCLUDE_ASM("asm/nonmatchings/level_init", CoordHash);
+int CoordHash(int x,int y,int z,int dir,int div,int mod) {
+    switch(dir) {
+    case 0:
+        z = z + -0x100;
+        break;
+    case 1:
+        x = x + 0x100;
+        break;
+    case 2:
+        y = y + 0x100;
+        break;
+    case 3:
+        y = y + -0x100;
+        break;
+    case 4:
+        x = x + -0x100;
+        break;
+    case 5:
+        z = z + 0x100;
+    }
+    return ((x + y + z) / (div * 3)) % mod;
+}
 
 int GetRandomTextureRotation(void) {
     int ret;
