168538bb5839c20eb8981c9bce17e0042ad5ee69
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 19:09:14 +0200

    Neaten

--- src/enemy.c
+++ src/enemy.c
@@ -22,15 +22,12 @@ INCLUDE_ASM("asm/nonmatchings/enemy", FUN_0003d758);
 INCLUDE_ASM("asm/nonmatchings/enemy", FUN_0003d8b8);
 
 void FUN_0003da18(Enemy *enemy) {
-    short sVar1;
-
-    (enemy->dir).vx = -(enemy->dir).vx;
-    (enemy->dir).vy = -(enemy->dir).vy;
-    sVar1 = (enemy->field1_0x8).vx;
-    (enemy->dir).vz = -(enemy->dir).vz;
-    (enemy->field1_0x8).vx = -sVar1;
-    (enemy->field1_0x8).vy = -(enemy->field1_0x8).vy;
-    (enemy->field1_0x8).vz = -(enemy->field1_0x8).vz;
+    enemy->dir.vx = -enemy->dir.vx;
+    enemy->dir.vy = -enemy->dir.vy;
+    enemy->dir.vz = -enemy->dir.vz;
+    enemy->field1_0x8.vx = -enemy->field1_0x8.vx;
+    enemy->field1_0x8.vy = -enemy->field1_0x8.vy;
+    enemy->field1_0x8.vz = -enemy->field1_0x8.vz;
 }
 
 int FUN_0003da64(Enemy *enemy) {
