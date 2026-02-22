e81973b150115a7b26cc43fdbeb135a0b8f5faf0
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 17:58:15 +0200

    Migrate AddDrChangePrims from pending_matches

--- src/render1.c
+++ src/render1.c
@@ -10,6 +10,27 @@ INCLUDE_ASM("asm/nonmatchings/render1", GetShortFromGgiPart2);
 
 INCLUDE_ASM("asm/nonmatchings/render1", RenderPlayerAndItems);
 
-INCLUDE_ASM("asm/nonmatchings/render1", AddDrChangePrims);
+extern DR_TPAGE drTpages1[2][1];
+extern DR_TPAGE drTpages2[2][1];
+extern int cameraIndex;
+extern DR_AREA drawAreas[2][1];
+extern DR_OFFSET drawOffsets[2][1];
+extern void* otag[2][1][1026];
+extern TgiFile * tgi;
+extern int whichDrawDispEnv;
+
+void AddDrChangePrims(void) {
+    if (tgi->skyboxFlag == 1025) {
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][1025], &drTpages1[whichDrawDispEnv][cameraIndex]);
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][1024], &drTpages2[whichDrawDispEnv][cameraIndex]);
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][1025], &drawAreas[whichDrawDispEnv][cameraIndex]);
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][1025], &drawOffsets[whichDrawDispEnv][cameraIndex]);
+    }
+    else {
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][tgi->skyboxFlag], &drTpages2[whichDrawDispEnv][cameraIndex]);
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][tgi->skyboxFlag], &drawAreas[whichDrawDispEnv][cameraIndex]);
+        addPrim(&otag[whichDrawDispEnv][cameraIndex][tgi->skyboxFlag], &drawOffsets[whichDrawDispEnv][cameraIndex]);
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/render1", UpdateStarfield);
