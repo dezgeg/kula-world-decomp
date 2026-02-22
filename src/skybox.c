596a1f4a2b6d5cf25bdded8129fe647a42d2b9d1
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-02-22 17:37:52 +0200

    Migrate Square from pending_matches

--- src/skybox.c
+++ src/skybox.c
@@ -4,6 +4,8 @@ INCLUDE_ASM("asm/nonmatchings/skybox", RecalcSkyboxes01);
 
 INCLUDE_ASM("asm/nonmatchings/skybox", RecalcSkyboxes2);
 
-INCLUDE_ASM("asm/nonmatchings/skybox", Square);
+int Square(int param_1) {
+    return param_1 * param_1;
+}
 
 INCLUDE_ASM("asm/nonmatchings/skybox", SkyboxSinCos);
--- work/pending_matches/Square.c
+++
@@ -1,6 +0,0 @@
-
-int Square(int param_1) {
-    return param_1 * param_1;
-}
-
-
