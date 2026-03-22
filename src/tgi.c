93443ee6a353438f9782246cd7844dc439c6926b
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-03-22 22:22:36 +0200

    Migrate ParseLevelDataFromTgi

--- src/tgi.c
+++ src/tgi.c
@@ -25,8 +25,15 @@ extern void InitTurningMotionBlur(void);
 extern void LoadImagesFromTgiPart9(short* p);
 
 // gprel-used variables (defined in this file)
+int DAT_000a4128;
+int itemsDispListIdx;
+int numPlainTileTextureVariations;
+int playerEnemyDispListIdx;
+int quadSomethingCount;
+int quadSomethingStartIndex;
 int recalcSkyboxes;
 TgiFile* tgi;
+int turningMotionBlurEnabled;
 short* tgiEnd;
 short* tgiPart0;
 short* tgiPart1;
@@ -48,6 +55,40 @@ extern int HIGHSCORE_CUBE_RANDOM_TEXTURES[16];
 extern int INT_ARRAY_000a2cd8[7];
 extern int LOD_THRESHOLDS[9];
 extern int turningMotionBlurTable[9 * 7];
+
+extern short DAT_00171314;
+extern short DAT_00171316[];
+extern short uncompLevelData[];
+extern short cubeIndexes[39304];
+extern void* cubeStates[256*8];
+extern int isFinal;
+extern int lightEffects[32 * 8];
+extern int recentlyVisitedCubeFaces[4 * 32];
+extern int sunglassDisablingState[1];
+extern int sunglassSeeEverything[1];
+extern int toBeDisabledLightEffects[64];
+extern int unkOtagLengths[2][1];
+
+short* entityData;
+short* levelData;
+short numEntities;
+int specialLevelType;
+int wasSpecialLevel;
+
+extern void DisableScreenShake(void);
+extern void InitLasers(void);
+extern void InitManySprites(void);
+extern void InitPlayerSpecularSprite(void);
+extern void InitShadowSprites(void);
+extern void ParseKeysAndSpecialLevelFromItemData(void);
+extern void ProcessLevelData(void);
+extern void RecalcSkyboxes01(int angleOfSomethingI, int angleOfSomethingJ, int countOfSomethingI, int countOfSomethingJ, int param_5);
+extern void RecalcSkyboxes2(void);
+extern void ResetCheats(void);
+extern void ResetVibration(void);
+extern void ScanLevelDataForBlinkingEntities(void);
+extern void SetupDisplay(int isbg, int bgR, int bgG, int bgB, int useDithering, int use24Bit);
+extern void TweakSkyboxColors(uint param_1);
 
 void* ParseTGI(TgiFile* tgiBuf) {
     int i;
@@ -227,4 +268,117 @@ void InitTurningMotionBlur(void) {
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/tgi", ParseLevelDataFromTgi);
+void ParseLevelDataFromTgi(void) {
+    int i;
+    int j;
+    unsigned k;
+    int* ptr;
+    int* ptr2;
+
+    levelData = (void*)0x0015e000;
+    entityData = (void*)0x00171316;
+    numEntities = *(short*)0x00171314;
+
+    InitLasers();
+    ParseKeysAndSpecialLevelFromItemData();
+    ResetVibration();
+    DisableScreenShake();
+
+    for (i = 0; i < 39304; i++) {
+        ((short*)0x1af000)[i] = -1;
+    }
+
+    ptr = cubeStates;
+    for (i = 0; i < 4096; i++) {
+        *ptr++ = -1;
+    }
+
+    if (specialLevelType == 1) {
+        numPlainTileTextureVariations = tgi->numPlainTileTextureVariationsSpecialLevel;
+        DAT_000a4128 = tgi->unk160;
+        quadSomethingStartIndex = tgi->unk150 + tgi->numPlainTileTextureVariationsNormalLevel + tgi->unk158;
+    } else {
+        quadSomethingStartIndex = 0;
+        numPlainTileTextureVariations = tgi->numPlainTileTextureVariationsNormalLevel;
+        DAT_000a4128 = tgi->unk158;
+    }
+
+    quadSomethingCount = tgi->unk150 + numPlainTileTextureVariations + DAT_000a4128;
+
+    ProcessLevelData();
+    InitManySprites();
+    ResetCheats();
+
+    for (i = 0; i < 32; i++) {
+        recentlyVisitedCubeFaces[4 * i] = (void*)-1;
+    }
+
+    itemsDispListIdx = 0;
+    playerEnemyDispListIdx = 0;
+
+    for (i = 0; i < 1; i++) {
+        sunglassSeeEverything[i] = 0;
+    }
+
+    for (i = 0; i < 1; i++) {
+        sunglassDisablingState[i] = 0;
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 1; j++) {
+            unkOtagLengths[i][j] = 32;
+        }
+    }
+
+    for (i = 0; i < 32; i++) {
+        lightEffects[8 * i] = 0xffffffff;
+    }
+
+    InitShadowSprites();
+    InitPlayerSpecularSprite();
+    ScanLevelDataForBlinkingEntities();
+
+    for (i = 0; i < 64; i++) {
+        toBeDisabledLightEffects[i] = -1;
+    }
+
+    turningMotionBlurEnabled = 0;
+    for (i = 0; i < 7; i++) {
+        tgi->lodDistance[i] = turningMotionBlurTable[0 * 7 + i];
+    }
+
+    if (tgi->skyboxFlag == 1025) {
+        if (specialLevelType != wasSpecialLevel || recalcSkyboxes == 1) {
+            switch (specialLevelType) {
+            case 0:
+                RecalcSkyboxes01(3, 5, 16, 5, 40);
+                break;
+            case 1:
+                RecalcSkyboxes01(4, 3, 16, 10, 36);
+                break;
+            case 2:
+                RecalcSkyboxes2();
+                break;
+            }
+            recalcSkyboxes = 0;
+        }
+    }
+
+    if (isFinal == 1) {
+        TweakSkyboxColors(0x787878);
+    }
+
+    if (tgi->skyboxFlag == 1025) {
+        if (specialLevelType == 2) {
+            SetupDisplay(1, 0, 0, 0, 0, 0);
+        } else {
+            SetupDisplay(0, 0, 0, 0, 0, 0);
+        }
+    } else {
+        if (specialLevelType == 1) {
+            SetupDisplay(1, tgi->specialLevelBgColor[0], tgi->specialLevelBgColor[1], tgi->specialLevelBgColor[2], 0, 0);
+        } else {
+            SetupDisplay(1, tgi->normalLevelBgColor[0], tgi->normalLevelBgColor[1], tgi->normalLevelBgColor[2], 0, 0);
+        }
+    }
+}
