13a8153a36b392c499b93eceb4cdb2dfdeb0cc17
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-04-03 19:54:55 +0300

    Reorder skybox.c

--- src/skybox.c
+++ src/skybox.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+int skyboxMaxAngle1;
+int skyboxMaxAngle2;
+
 INCLUDE_ASM("asm/nonmatchings/skybox", RecalcSkyboxes01);
 
 INCLUDE_ASM("asm/nonmatchings/skybox", RecalcSkyboxes2);
@@ -7,9 +10,6 @@ INCLUDE_ASM("asm/nonmatchings/skybox", RecalcSkyboxes2);
 int Square(int param_1) {
     return param_1 * param_1;
 }
-
-int skyboxMaxAngle1;
-int skyboxMaxAngle2;
 
 void SkyboxSinCos(int angleval1, int angleval2, short *out1, short *out2, short *out3) {
     int cosParam2;
