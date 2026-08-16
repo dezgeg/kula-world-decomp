916bfe34380ff77dded9d8d5369465b7579e7e59
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-08-16 03:36:37 +0300

    Var prep

--- src/score_graph.c
+++ src/score_graph.c
@@ -1,5 +1,22 @@
 #include "common.h"
 
+extern void TSpritePrim(TSprite * ts, int dfe, int dtd, int tpage);
+
+extern int whichDrawDispEnv;
+extern PrimList primLists[2];
+extern Texture textures[150];
+extern TSprite scoreGraphSprites[2][2];
+
+int doDrawGraph;
+int drawScoreGraphHeight;
+int* drawScoreGraphHeightsBuf;
+int* drawScoreGraphLevelScores;
+int* drawScoreGraphUnusedScorePtr;
+int drawScoreGraphWidth;
+int drawScoreGraphY;
+void* drawScoreGraphPrims;
+uint firstGuiTexture;
+
 INCLUDE_ASM("asm/nonmatchings/score_graph", InitScoreGraph);
 
 INCLUDE_ASM("asm/nonmatchings/score_graph", DrawScoreGraph);
