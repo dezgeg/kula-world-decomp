da1f60042484f66323b2ab18ed097d1c09d9b8f3
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-09-05 00:35:40 +0300

    Skip unused to ASM

--- src/asm_hacks1.c
+++ src/asm_hacks1.c
@@ -1,3 +1,5 @@
 #include "common.h"
 
+#ifndef SKIP_UNUSED_CODE
 INCLUDE_ASM("asm/nonmatchings/asm_hacks1", CheckIsDevkitBios);
+#endif
--- src/asm_hacks2.c
+++ src/asm_hacks2.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#ifndef SKIP_UNUSED_CODE
 INCLUDE_ASM("asm/nonmatchings/asm_hacks2", UnusedDivBy0);
 
 INCLUDE_ASM("asm/nonmatchings/asm_hacks2", divZeroCounter);
+#endif
